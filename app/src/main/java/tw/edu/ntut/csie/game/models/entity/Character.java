9bedeca1cc7638e4fb3cdbbd6cb7a758edf5a486
HoShuHang <kwite2002@gmail.com>
2016-01-12 15:08:36 +0800

    add unit test in CharacterTest

--- app/src/main/java/tw/edu/ntut/csie/game/models/entity/Character.java
+++ app/src/main/java/tw/edu/ntut/csie/game/models/entity/Character.java
@@ -1,6 +1,6 @@
 package tw.edu.ntut.csie.game.models.entity;
 
-public class Character {
+public class Character{
     public boolean testReturnTrue(){
         return true;
     }
--- app/src/test/java/tw/edu/ntut/csie/game/models/entity/CharacterTest.java
+++ app/src/test/java/tw/edu/ntut/csie/game/models/entity/CharacterTest.java
@@ -2,7 +2,11 @@ package tw.edu.ntut.csie.game.models.entity;
 
 import junit.framework.TestCase;
 
+import java.lang.reflect.Field;
+
 public class CharacterTest extends TestCase {
+    private final int MAP_WIDTH = 2400;
+    private final int CHARACTER_WIDTH = 80;
     @Override
     protected void setUp() throws Exception {
         super.setUp();
@@ -10,7 +14,46 @@ public class CharacterTest extends TestCase {
 
     public void test(){
         Character c = new Character();
-        assert(c.testReturnTrue());
+        assertTrue(c.testReturnTrue());
+    }
+
+    public void testGetLocation(){
+        Character c = new Character();
+        assertTrue(c.getLocation() == 0);
+        try {
+            Field f = Character.class.getDeclaredField("_location");
+            f.setAccessible(true);
+            f.setInt(c, 20);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        assertTrue(c.getLocation() == 20);
+    }
+
+    public void testMoveRight(){
+        Character c = new Character();
+        assertTrue(c.getLocation() == 0);
+        c.move(10);
+        assertTrue(c.getLocation() == 10);
+        c.move(-20);
+        assertTrue(c.getLocation() == 0);
+        c.move(2400);
+        assertTrue(c.getLocation() == MAP_WIDTH - CHARACTER_WIDTH);
+    }
+
+    public void testMoveLeft(){
+        Character c = new Character();
+        assertTrue(c.getLocation() == 0);
+        c.move(-10);
+        assertTrue(c.getLocation() == 0);
+        c.move(100);
+        assertTrue(c.getLocation() == 100);
+        c.move(-50);
+        assertTrue(c.getLocation() == 50);
+        c.move(-60);
+        assertTrue(c.getLocation() == 0);
     }
 
     @Override
